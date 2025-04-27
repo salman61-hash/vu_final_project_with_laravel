e7f3413e34db2d3aba3551ddf5e62ee9a95acdbe
salman61-hash <salmanidb61@gmail.com>
2025-04-27 10:15:25 +0600

    Solved

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     vueDevTools(),
   ],
 
-  // base:'https://devsalman.com/vue-laravel',
+  base:'https://devsalman.com/vue-laravel',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
